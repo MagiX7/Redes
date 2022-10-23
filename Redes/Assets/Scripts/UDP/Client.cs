e4948f1c489ee746e1ee39efd6e96260488e268e
David González <DavidGonzalez2001-9@hotmail.com>
2022-10-23 11:08:29 +0200

    Client removed comments + no errors when quitting application

--- Redes/Assets/Scripts/UDP/Client.cs
+++ Redes/Assets/Scripts/UDP/Client.cs
@@ -13,46 +13,41 @@ public class Client : MonoBehaviour
     Socket clientSocket;
     IPEndPoint clientIpep;
 
-    int recv = 0; // Size of the data
+    int recv = 0;
     byte[] data;
     public string clientIp;
     public string serverIp;
     [HideInInspector] public string userName;
     
     EndPoint remote = null;
-    IPEndPoint sender;
 
-    Thread netThread;
-    //Thread sendMsgsThread;
+    Thread receiveMsgsThread;
+
     bool finished = false;
     bool newMessage = false;
-    //bool messageSend = false;
 
-    string text;
+    string incomingText;
 
     [SerializeField] Text chat;
     [SerializeField] InputField input;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         clientIpep = new IPEndPoint(IPAddress.Parse(GetLocalIPAddress()), 5345);
         clientSocket.Bind(clientIpep);
 
-        sender = new IPEndPoint(IPAddress.Parse(serverIp), 5345);
-        remote = (EndPoint)(sender);
+        remote = new IPEndPoint(IPAddress.Parse(serverIp), 5345);
 
         data = new byte[1024];
         data = Encoding.ASCII.GetBytes(userName);
         clientSocket.SendTo(data, data.Length, SocketFlags.None, remote);
         data = new byte[1024];
 
-        netThread = new Thread(RecieveMessages);
-        netThread.Start();
+        receiveMsgsThread = new Thread(RecieveMessages);
+        receiveMsgsThread.Start();
 
-        //sendMsgsThread = new Thread(OnMessageSent);
-        //sendMsgsThread.Start();
     }
 
     private void OnDisable()
@@ -60,26 +55,20 @@ public class Client : MonoBehaviour
         finished = true;
 
         clientSocket.Close();
-        if (netThread.IsAlive)
-            netThread.Abort();
+        if (receiveMsgsThread.IsAlive)
+            receiveMsgsThread.Abort();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            finished = true;
-        }
-
         if (newMessage)
         {
-            chat.text += (text + "\n");
+            chat.text += (incomingText + "\n");
             newMessage = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            //messageSend = true;
             OnMessageSent();
         }
     }
@@ -88,46 +77,16 @@ public class Client : MonoBehaviour
     {
         while (!finished)
         {
-            try
-            {
-                byte[] msg = new byte[1024];
-                recv = clientSocket.ReceiveFrom(msg, SocketFlags.None, ref remote);
-                text = Encoding.ASCII.GetString(msg, 0, recv);
-                newMessage = true;
-                data = msg;
-            }
-            catch (Exception e)
-            {
-                Debug.Log("Error when sending the message: " + e);
-            }
+            byte[] msg = new byte[1024];
+            recv = clientSocket.ReceiveFrom(msg, SocketFlags.None, ref remote);
+            incomingText = Encoding.ASCII.GetString(msg, 0, recv);
+            newMessage = true;
+            data = msg;
         }
     }
 
     void OnMessageSent()
     {
-        //while (!finished)
-        //{
-        //    if (messageSend)
-        //    {
-        //        try
-        //        {
-        //            string msg = "[" + userName + "]" + ": " + input.text;
-        //            data = Encoding.ASCII.GetBytes(msg);
-        //            recv = data.Length;
-        //            clientSocket.SendTo(data, recv, SocketFlags.None, remote);
-        //            input.text = "";
-        //            messageSend = false;
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Debug.Log("Error when sending the message: " + e);
-        //        }
-
-        //        messageSend = false;
-        //    }
-        //}
-
-
         string msg = "[" + userName + "]" + ": " + input.text;
         data = Encoding.ASCII.GetBytes(msg);
         recv = data.Length;
