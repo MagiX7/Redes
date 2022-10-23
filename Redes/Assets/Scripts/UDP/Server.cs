76859719916469627a83043d0cb02aa1637b197d
David González <DavidGonzalez2001-9@hotmail.com>
2022-10-23 11:02:42 +0200

    Server removed comments + no warning when exiting application

--- Redes/Assets/Scripts/UDP/Server.cs
+++ Redes/Assets/Scripts/UDP/Server.cs
@@ -14,20 +14,17 @@ public class Server : MonoBehaviour
 {
     Socket serverSocket;
     IPEndPoint ipep;
-    int recv = 0; // Size of the data
+    int recv = 0;
     byte[] data;
 
     EndPoint remote = null;
 
     Thread receiveMsgsThread;
-    //Thread sendMsgsThread;
-    //Thread updateMsgsThread;
     bool finished = false;
 
     bool newMessage = false;
     bool messageSent = false;
-    //bool sendingMsg = false;
-    //bool msgReceived = false;
+
     bool clientConnected = false;
     string lastUserName = string.Empty;
 
@@ -53,12 +50,6 @@ public class Server : MonoBehaviour
         receiveMsgsThread = new Thread(RecieveMessages);
         receiveMsgsThread.Start();
 
-        //sendMsgsThread = new Thread(OnMessageSent);
-        //sendMsgsThread.Start();
-
-        //updateMsgsThread = new Thread(OnMessageReceived);
-        //updateMsgsThread.Start();
-
         connectedPeople.text += ("You (Server)\n");
     }
     
@@ -66,26 +57,17 @@ public class Server : MonoBehaviour
     {
         finished = true;
 
+        remoters.Clear();
+
         serverSocket.Close();
         if (receiveMsgsThread.IsAlive)
             receiveMsgsThread.Abort();
-
-        //if (sendMsgsThread.IsAlive)
-        //    sendMsgsThread.Abort();
-        //if (updateMsgsThread.IsAlive)
-        //    updateMsgsThread.Abort();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            finished = true;
-        }
-
         if (!clientConnected && newMessage)
         {
-            //msgReceived = true;
             OnMessageReceived();
         }
         else if (clientConnected)
@@ -95,7 +77,6 @@ public class Server : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            //sendingMsg = true;
             OnMessageSent();
         }
         if (messageSent)
@@ -111,69 +92,37 @@ public class Server : MonoBehaviour
     {
         while (!finished)
         {
-            try
+            byte[] bytes = new byte[1024];
+            recv = serverSocket.ReceiveFrom(bytes, SocketFlags.None, ref remote);
+            if (recv > 0)
             {
-                byte[] msg = new byte[1024];
-                recv = serverSocket.ReceiveFrom(msg, SocketFlags.None, ref remote);
-                if (recv > 0)
+                text = Encoding.ASCII.GetString(bytes, 0, recv);
+                newMessage = true;
+                data = bytes;
+
+                if (!remoters.Contains(remote))
                 {
-                    text = Encoding.ASCII.GetString(msg, 0, recv);
-                    newMessage = true;
-                    data = msg;
+                    clientConnected = true;
+                    lastUserName = text;
+                    remoters.Add(remote);
 
-                    if (!remoters.Contains(remote))
+                    for (int i = 0; i < remoters.Count; i++)
                     {
-                        clientConnected = true;
-                        lastUserName = text;
-                        remoters.Add(remote);
+                        if (remote == remoters[i])
+                            text = "Welcome to the UDP server";
+                        else
+                            text = lastUserName + " Connected!\n";
 
-                        for (int i = 0; i < remoters.Count; i++)
-                        {
-                            if (remote == remoters[i])
-                                text = "Welcome to the UDP server";
-                            else
-                                text = lastUserName + " Connected!\n";
-
-                            msg = Encoding.ASCII.GetBytes(text);
-                            serverSocket.SendTo(msg, msg.Length, SocketFlags.None, remoters[i]);
-                        }
+                        bytes = Encoding.ASCII.GetBytes(text);
+                        serverSocket.SendTo(bytes, bytes.Length, SocketFlags.None, remoters[i]);
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                Debug.Log("Error when receiving a message: " + e);
             }
         }
     }
 
     void OnMessageReceived()
     {
-        //while (!finished)
-        //{
-        //    if (msgReceived)
-        //    {
-        //        try
-        //        {
-        //            data = Encoding.ASCII.GetBytes(text);
-        //            recv = data.Length;
-
-        //            for (int i = 0; i < remoters.Count; i++)
-        //                serverSocket.SendTo(data, recv, SocketFlags.None, remoters[i]);
-
-        //            chat.text += (text + "\n");
-
-        //            newMessage = false;
-        //            msgReceived = false;
-        //            data = new byte[1024];
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Debug.Log("Error when sending message: " + e);
-        //        }
-        //    }
-        //}
-
         data = Encoding.ASCII.GetBytes(text);
         recv = data.Length;
 
@@ -188,31 +137,6 @@ public class Server : MonoBehaviour
 
     void OnMessageSent()
     {
-        //while (!finished)
-        //{
-        //    if (sendingMsg)
-        //    {
-        //        try
-        //        {
-        //            data = Encoding.ASCII.GetBytes("[Server]: " + input.text);
-        //            recv = data.Length;
-        //            for (int i = 0; i < remoters.Count; i++)
-        //            {
-        //                serverSocket.SendTo(data, recv, SocketFlags.None, remoters[i]);
-        //            }
-        //            messageSent = true;
-        //            sendingMsg = false;
-        //            chat.text += ("Server: " + input.text + "\n");
-        //            input.text = "";
-        //            data = new byte[1024];
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Debug.Log("Error when sending message: " + e);
-        //        }
-        //    }
-        //}
-
         data = Encoding.ASCII.GetBytes("[Server]: " + input.text);
         recv = data.Length;
         for (int i = 0; i < remoters.Count; i++)
@@ -226,23 +150,6 @@ public class Server : MonoBehaviour
 
     void OnClientConnected()
     {
-        //byte[] msg;
-        //string text = string.Empty;
-        //for (int i = 0; i < remoters.Count; i++)
-        //{
-        //    if (remote == remoters[i])
-        //    {
-        //        text = "Welcome to the UDP server";
-        //    }
-        //    else
-        //    {
-        //        text = lastUserName + " Connected!\n";
-        //    }
-        //
-        //    msg = Encoding.ASCII.GetBytes(text);
-        //    serverSocket.SendTo(msg, msg.Length, SocketFlags.None, remoters[i]);
-        //}
-
         chat.text += (lastUserName + " Connected!\n");
         connectedPeople.text += (lastUserName + "\n");
 
