3c019de4f5f87b3e7b05b534984ef75c47e14ade
Lucas Pérez <lucaspg14@gmail.com>
2022-11-24 08:21:57 +0100

    Unnecessary lines on two scripts

--- Redes/Assets/Scripts/UDP/ServerUDP.cs
+++ Redes/Assets/Scripts/UDP/ServerUDP.cs
@@ -6,7 +6,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
